cbf3f1760c6050b6d7553966c8be4179213ba072
Ejiro404 <ejiroetagbemukwe@gmail.com>
2025-10-21 11:55:16 +0100

    Fixed deployment target to personal repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/FAVICON.png',
 
   // Set the production url of your site here
-  url: 'https://Ejiro404.github.io',
+  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Projects-Documentations/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
